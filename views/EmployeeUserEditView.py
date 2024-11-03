8076cc0c7de8453a23790f1926570b761c265ca8
Gustavo Beretta Gonçalves <gustavoberettag@gmail.com>
2024-11-03 15:26:51 -0300

    refactor commit anterior

--- views/EmployeeUserEditView.py
+++ views/EmployeeUserEditView.py
@@ -204,10 +204,7 @@ class EmployeeUserEditView:
         weight = self.weight_entry.get()
         height = self.height_entry.get()
 
-        try:
-            self.controller.update_user_as_employee(self.user, full_name, new_password, password_confirmation, gender, int(us_shoe_size), int(age), is_employee, int(weight), int(height))
-        except:
-            self.show_message('Error', 'Make sure to fill correctly the fields that request numbers')
+        self.controller.update_user_as_employee(self.user, full_name, new_password, password_confirmation, gender, us_shoe_size, age, is_employee, weight, height)
 
     def working_historic(self):
         messagebox.showinfo("Info", "Working History clicked")
--- views/GuestEditView.py
+++ views/GuestEditView.py
@@ -149,10 +149,7 @@ class GuestEditView:
         weight = self.weight_entry.get()
         height = self.height_entry.get()
 
-        try:
-            self.controller.update_user_as_guest(self.user, full_name, new_password, password_confirmation, gender, int(us_shoe_size), int(age), int(weight), int(height))
-        except:
-            self.show_message('Error', 'Make sure to fill correctly the fields that request numbers')
+        self.controller.update_user_as_guest(self.user, full_name, new_password, password_confirmation, gender, us_shoe_size, age, weight, height)
 
     def show_message(self, title, message):
         messagebox.showinfo(title, message)
