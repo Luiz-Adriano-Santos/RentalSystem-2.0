85f4ee8201c3f557000262fd38ad8567a95fb13b
Josef Bruseke <josefbruseke@gmail.com>
2024-11-04 19:48:31 -0300

    fix db creation

--- models/Equipment.py
+++ models/Equipment.py
@@ -2,19 +2,19 @@ import sqlite3
 import pickle
 
 class Equipment:
-    def __init__(self, equipment_id, equipment_type, size, registration_date, availability):
+    def __init__(self, equipment_id, equipment_type, size, registration_date):
         self.equipment_id = equipment_id
         self.equipment_type = equipment_type
         self.size = size
         self.registration_date = registration_date
-        self.availability = availability
+        self.availability = 'Available'
 
 
     def create_equipment(self):
         conn = sqlite3.connect('RentalSystem.db')
         cursor = conn.cursor()
         
-        cursor.execute("INSERT INTO equipments (equipment_id, equipment) VALUES (?, ?)", 
+        cursor.execute("INSERT INTO equipments (id, equipment) VALUES (?, ?)", 
                    (self.equipment_id, pickle.dumps(self)))
 
         conn.commit()
