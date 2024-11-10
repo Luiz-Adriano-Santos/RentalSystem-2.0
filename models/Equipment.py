108586608c1c18d5780e157ce93a290c84a78e1c
Josef Bruseke <josefbruseke@gmail.com>
2024-11-10 15:54:07 -0300

    add select all equipment query

--- models/Equipment.py
+++ models/Equipment.py
@@ -9,7 +9,6 @@ class Equipment:
         self.registration_date = registration_date
         self.availability = 'Available'
 
-
     def create_equipment(self):
         conn = sqlite3.connect('RentalSystem.db')
         cursor = conn.cursor()
@@ -19,3 +18,20 @@ class Equipment:
 
         conn.commit()
         conn.close()
+
+    @staticmethod
+    def get_all_equipments():
+        conn = sqlite3.connect('RentalSystem.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM equipments")
+        all_equipments = cursor.fetchall()
+
+        conn.close()
+    
+        equipments_list = []
+        for equipment_data in all_equipments:
+            equipment = pickle.loads(equipment_data[1])
+            equipments_list.append(equipment)
+
+        return equipments_list
