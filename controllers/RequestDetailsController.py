a6e0622e200cdf19b5fe54bf9a7fc456ec3f2135
Josef Bruseke <josefbruseke@gmail.com>
2024-12-11 20:57:28 -0300

    change view order

--- controllers/RequestDetailsController.py
+++ controllers/RequestDetailsController.py
@@ -9,10 +9,12 @@ class RequestDetailsController:
         self.user = user
         self.request = request
         self.logged_employee = logged_employee
-        self.view = RequestDetailsView(self, request).mainloop()
+        
         self.equipmentDAO = EquipmentDAO()
         self.requestDAO = RequestDAO()
-    
+        
+        self.view = RequestDetailsView(self, request).mainloop()
+
     def employee_home_page(self):
         self.employee_home_controller.open_employee_home_page(self.user)
         self.equipmentDAO.conn.close()
